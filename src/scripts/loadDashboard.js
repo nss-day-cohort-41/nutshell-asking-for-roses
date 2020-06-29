f5acdec0aa214443a3b30b89f0ff01bc10159b68
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-29 16:40:10 -0500

    merging with new master

--- src/scripts/loadDashboard.js
+++ src/scripts/loadDashboard.js
@@ -2,22 +2,20 @@
 const container2= document.querySelector(".container2")
 const hiddenDashboard = document.querySelector(".container")
    hiddenDashboard.style.display = "none"
-  //let loadAll= document.getElementById(".container")
    
+
+   //registration completed and reseting DOM to display dashboard
+   //STILL REQUIRED populate dashboard list elements with user data.
 const userLoginSuccessful = document.querySelector(".login")
 userLoginSuccessful.addEventListener("click", event => { 
-   // addAll= window.location.assign (".tasksList")
-
    container2.style.display = "none"
-    //clearAll= document.querySelector(".container").innerHTML = loadAll.innerHTML
     hiddenDashboard.style.display = "block"
 
-    //add click event listener to anchor tag and after clicked display registration form
-    // registrationLink.addEventListener("click", clickEvent => {
-    
+  
 })
 
-   
+   //user requested to log out, reset DOM to registration page
+   //STILL REQUIRED clear user session
 const userLogOutRequest = document.querySelector(".logOutButton")
 userLogOutRequest.addEventListener("click", event => { 
     hiddenDashboard.style.display = "none"
