e60eefed93cce4989c55fa83b74e0fd5266b0224
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-29 13:20:38 -0500

    hide registration and show dashboard component framework is completed

---
+++ src/scripts/loadDashboard.js
@@ -0,0 +1,16 @@
+const hiddenDashboard = document.querySelector(".container")
+   hiddenDashboard.style.display = "none"
+  //let loadAll= document.getElementById(".container")
+   
+const x = document.querySelector(".login")
+x.addEventListener("click", event => { 
+   // addAll= window.location.assign (".tasksList")
+
+   let clearAll= document.querySelector(".container2").innerHTML = ""
+    //clearAll= document.querySelector(".container").innerHTML = loadAll.innerHTML
+    hiddenDashboard.style.display = "block"
+
+    //add click event listener to anchor tag and after clicked display registration form
+    // registrationLink.addEventListener("click", clickEvent => {
+    
+})
