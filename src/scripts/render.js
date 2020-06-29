794eed2cbb2ef73eeb278e0886ba66155bbe4691
David Larsen <davidplarsen@gmail.com>
2020-06-29 14:15:02 -0500

    Add render modules for additional sections

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -9,7 +9,7 @@ const renderToDom = {
     // Render the list of messages in the chat window
     messagesList() {
         // **TEST DATA - delete later
-        const messageArray = [{
+        const messagesArray = [{
             name: "test1",
             date: "2020-06-05"
         }, {
@@ -22,20 +22,59 @@ const renderToDom = {
         // **
 
         // Sort list of messages by date and render each message to the DOM
-        messageArray.sort((message1, message2) => new Date(message2.date) - new Date(message1.date))
-        console.log(messageArray) // Remove this line later
-        messageArray.forEach(message => {
+        messagesArray.sort((message1, message2) => new Date(message2.date) - new Date(message1.date))
+        console.log(messagesArray) // Remove this line later
+        messagesArray.forEach(message => {
             const messageHTML = domObject.messageComponent(message)
             document.querySelector(".messagesList").innerHTML += messageHTML
         })
     },
 
-    tasksList(tasksArray) {
+    tasksList() {
+        // **TEST DATA - delete later
+        const tasksArray = [{
+            name: "test1",
+            date: "2020-06-05"
+        }, {
+            name: "test2",
+            date: "2020-05-04"
+        }, {
+            name: "test3",
+            date: "2020-06-21"
+        }]
+        // **
+        const completeTasks = tasksArray.map(task => return task.completed = true)
+        const incompleteTasks = tasksArray.map(task => return task.completed = false)
+
+        tasksArray.sort((task1, task2) => new Date(task2.date) - new Date(task1.date))
+        console.log(tasksArray) // Remove this line later
+        tasksArray.forEach(task => {
+            const taskHTML = domObject.taskComponent(task)
+            document.querySelector(".tasksList").innerHTML += taskHTML
+        })
 
     },
 
     eventsList(eventsArray) {
+            // **TEST DATA - delete later
+            const eventsArray = [{
+                name: "test1",
+                date: "2020-06-05"
+            }, {
+                name: "test2",
+                date: "2020-05-04"
+            }, {
+                name: "test3",
+                date: "2020-06-21"
+            }]
+            // **
 
+            eventsArray.sort((event1, event2) => new Date(event2.date) - new Date(event1.date))
+            console.log(eventsArray) // Remove this line later
+            eventsArray.forEach(event => {
+                const eventHTML = domObject.eventComponent(event)
+                document.querySelector(".eventsList").innerHTML += eventHTML
+            })
     },
 
     articlesList(articlesArray) {
