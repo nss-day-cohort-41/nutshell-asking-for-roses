0d1b77fc4736ea2cab245ca0f179a4aa0f4bdb5f
Brett Stoudt <Bstoudt01@gmail.com>
2020-06-30 08:55:11 -0500

    added login to dsashboard click event to main.js. also moved the dashboard container over to index.html

--- src/scripts/loadDashboard.js
+++ src/scripts/loadDashboard.js
@@ -1,9 +1,9 @@
 
-const container2= document.querySelector(".container2")
-const hiddenDashboard = document.querySelector(".container")
+const container2= document.querySelector("#registrationContainer")
+const hiddenDashboard = document.querySelector("#dashboardContainer")
    hiddenDashboard.style.display = "none"
    
-
+   // place in registation click events final else statement
    //registration completed and reseting DOM to display dashboard
    //STILL REQUIRED populate dashboard list elements with user data.
 const userLoginSuccessful = document.querySelector(".login")
@@ -13,7 +13,7 @@ userLoginSuccessful.addEventListener("click", event => {
 
   
 })
-
+   //place in main js ?
    //user requested to log out, reset DOM to registration page
    //STILL REQUIRED clear user session
 const userLogOutRequest = document.querySelector(".logOutButton")
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,29 @@ userRegistration.registeredUserLogin();
 renderToDom.messagesList()
 renderToDom.tasksList()
 renderToDom.eventsList()
+
+
+const registrationContainer= document.querySelector("#registrationContainer")
+const hiddenDashboard = document.querySelector("#dashboardContainer")
+   hiddenDashboard.style.display = "none"
+   
+   // place in registation click events final else statement
+   //registration completed and reseting DOM to display dashboard
+   //STILL REQUIRED populate dashboard list elements with user data.
+const userLoginSuccessful = document.querySelector(".registerButton")
+userLoginSuccessful.addEventListener("click", event => { 
+    registrationContainer.style.display = "none"
+    hiddenDashboard.style.display = "block"
+
+  
+})
+   //place in main js ?
+   //user requested to log out, reset DOM to registration page
+   //STILL REQUIRED clear user session
+const userLogOutRequest = document.querySelector(".logOutButton")
+userLogOutRequest.addEventListener("click", event => { 
+    hiddenDashboard.style.display = "none"
+    registrationContainer.style.display = "block"
+
+    
+})
